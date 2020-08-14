1642d807938d54199e534ff1cd4e654afd6ae5c3
Varna <bharath.varna@marriott-sp.com>
2020-08-14 13:19:12 -0400

    Test Commit

---
+++ src/test/java/com/bvarna/springbootdemo/SpringBootDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bvarna.springbootdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
